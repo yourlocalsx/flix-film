925d7d8ebb999e871afaa846d20268fee5c41c2e
Joeytjuhh <79990012+Joeytjuhh@users.noreply.github.com>
2024-06-03 09:31:30 +0200

    Removed twitter link

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -81,9 +81,6 @@ export function Footer() {
           <FooterLink icon={Icons.DISCORD} href={conf().DISCORD_LINK}>
             {t("footer.links.discord")}
           </FooterLink>
-          <FooterLink icon={Icons.TWITTER} href={conf().TWITTER_LINK}>
-            {t("footer.links.twitter")}
-          </FooterLink>
           <div className="inline md:hidden">
             <Dmca />
           </div>
