6a0249bf6877577b0bac2dafca39033a983deb3d
Joeytjuhh <hi@example.org>
2024-06-10 10:00:23 +0200

    English oui oui

--- src/assets/languages.ts
+++ src/assets/languages.ts
@@ -1,7 +1,9 @@
 import lan from "@/assets/locales/lan.json";
+import en from "@/assets/locales/lan.json";
 
 export const locales = {
   lan,
+  en,
 };
 
 export type Locales = keyof typeof locales;
