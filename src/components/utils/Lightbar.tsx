9496149518cc8d182d38b39f0bbc3901a7409f70
Joeytjuhh <hi@example.org>
2024-06-17 09:10:36 +0200

    updated christmas icons

--- src/components/utils/Lightbar.tsx
+++ src/components/utils/Lightbar.tsx
@@ -164,7 +164,7 @@ function ParticlesCanvas() {
             sizeRange: [25, 35] as [number, number],
           },
         ];
-        imageParticleCount = particleCount * 0.1;
+        imageParticleCount = particleCount * 0.8;
         break;
 
       default:
