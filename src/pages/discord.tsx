06f5ab52698d3bba6d4f4e427007dd94ad1a8e05
Joeytjuhh <79990012+Joeytjuhh@users.noreply.github.com>
2024-07-29 00:54:39 +0200

    delete

--- src/pages/discord.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function DiscordPage() {
-  useEffect(() => {
-    window.location.href = "https://discord.com/";
-  }, []);
-
-  return null;
-}
